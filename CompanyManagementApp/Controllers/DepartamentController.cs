e4da06f55f1449610992b3054fb69fc450dee2f1
anarjr <anarjr@code.edu.az>
2026-07-10 04:05:16 +0400

    Add Departament Controller

--- CompanyManagementApp/Controllers/DepartamentController.cs
+++ CompanyManagementApp/Controllers/DepartamentController.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿using Domain.Models;
+using Services.Services;
+using Services.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CompanyManagementApp.Controllers
 {
-    internal class DepartamentController
+    public class DepartmentController
     {
+        private readonly IDepartmentService _departmentService;
+
+        public DepartmentController()
+        {
+            _departmentService = new DepartmentService();
+        }
+
+
+        public void Create(Departament department)
+        {
+            _departmentService.Create(department);
+        }
+
+
+        public void Update(Departament department)
+        {
+            _departmentService.Update(department);
+        }
+
+        public void Delete(int id)
+        {
+            _departmentService.Delete(id);
+        }
+
+        public Departament GetById(int id)
+        {
+            return _departmentService.GetById(id);
+        }
+
+        public List<Departament> GetAll()
+        {
+            return _departmentService.GetAll();
+        }
+
+        public List<Departament> Search(string searchText)
+        {
+            return _departmentService.Search(searchText);
+        }
+
+        public List<Departament> SortByCapacity()
+        {
+            return _departmentService.SortByCapacity();
+        }
     }
 }
