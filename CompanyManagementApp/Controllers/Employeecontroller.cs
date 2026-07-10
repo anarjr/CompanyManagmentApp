b0b6bf9ebf8fa7833b2fe693cb1c8236413c68bd
anarjr <anarjr@code.edu.az>
2026-07-10 04:12:20 +0400

    Employee Controller

--- CompanyManagementApp/Controllers/Employeecontroller.cs
+++ CompanyManagementApp/Controllers/Employeecontroller.cs
@@ -1,10 +1,69 @@
-﻿using System;
+﻿using Domain.Models;
+using Services.Services;
+using Services.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CompanyManagementApp.Controllers
 {
-    internal class Employeecontroller
+    public class EmployeeController
     {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController()
+        {
+            _employeeService = new EmployeeService();
+        }
+
+        public void Create(Employee employee)
+        {
+            _employeeService.Create(employee);
+        }
+
+        public void Update(Employee employee)
+        {
+            _employeeService.Update(employee);
+        }
+
+        public void Delete(int id)
+        {
+            _employeeService.Delete(id);
+        }
+
+        public Employee GetById(int id)
+        {
+            return _employeeService.GetById(id);
+        }
+
+        public List<Employee> GetAll()
+        {
+            return _employeeService.GetAll();
+        }
+
+        public List<Employee> GetEmployeesByAge(int age)
+        {
+            return _employeeService.GetEmployeesByAge(age);
+        }
+
+        public List<Employee> GetEmployeesByDepartmentId(int departmentId)
+        {
+            return _employeeService.GetEmployeesByDepartmentId(departmentId);
+        }
+
+        public List<Employee> GetEmployeesByDepartmentName(string departmentName)
+        {
+            return _employeeService.GetEmployeesByDepartmentName(departmentName);
+        }
+
+        public List<Employee> SearchByNameOrSurname(string searchText)
+        {
+            return _employeeService.SearchByNameOrSurname(searchText);
+        }
+
+        public int GetAllCount()
+        {
+            return _employeeService.GetAllCount();
+        }
     }
 }
